6e18b01d968fa8408e8b98a12294d9b1866ee173
Dhruv Saini <dhruvsaini0125@gmail.com>
2025-11-23 02:24:33 +0530

    Remove duplicate folders 1..6

--- 3/script.js
+++
@@ -1,7 +0,0 @@
-const cursor = document.querySelector(".cursor");
-const main = document.querySelector("main");
-main.addEventListener("mousemove",(e)=>{
-    cursor.style.left = `${e.x}px`;
-    cursor.style.top = `${e.y}px`;
-    
-})
